6d74684a911ccb8fefadcbd0a0f855007fb2cbeb
CodingRacoon94 <andreacerni@live.it>
2024-09-30 15:18:12 +0200

    inizializzazione progetto con livewire

---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,4 @@
+<div><!-- tutto quello che è dentro questo componente di livewire DEVE STARE SOLO DENTRO QUESTO DIV-->
+<!--In questo caso, abbiamo creato il componente cuounter con lo specifico comando nel terminale : php artisan make: livewire counter-->
+  <h2 class="text-primary">Il tuo componente livewire!</h2>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="text-center fs-1 text-danger text-decoration-underline display-1">Livewire prova</h1>
+</x-layout>
